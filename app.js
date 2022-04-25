39f364d1601f872c57abb7ce60a0cf8284580631
ppijn <pepijnjong2000@gmail.com>
2022-04-25 15:52:41 +0200

    MAde changes to the styling and implemented the API

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ const app = express();
 const http = require("http").createServer(app);
 const path = require("path");
 const io = require("socket.io")(http);
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 const supabase = require("./supabase.js");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.get("/", async (req, res) => {
-//   console.log("HOI");
-//   // const { data, error } = await supabase.from("drawing-words").select();
-//   // console.log(data);
-//   res.sendFile(path.resolve(__dirname, "public/index.html"));
-// });
+let wordArray;
+
+app.get("/", async (req, res) => {
+  console.log("HOI");
+  wordArray = await supabase.from("drawing-words").select();
+  console.log(wordArray.data);
+  res.sendFile(path.resolve(__dirname, "views/index.html"));
+
+  let randomWord = Math.floor(Math.random() * wordArray.data.length);
+
+  console.log(wordArray.data[randomWord]);
+});
 
 io.on("connection", (socket) => {
   console.log("a user connected");
@@ -30,3 +36,4 @@ io.on("connection", (socket) => {
 http.listen(port, () => {
   console.log("listening on port ", port);
 });
+// Get Radnom Word
