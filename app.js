394f4dbfdb68c7c313d856432965585d30d4362a
ppijn <pepijnjong2000@gmail.com>
2022-04-26 13:48:30 +0200

    Lots of socket io changes

--- app.js
+++ app.js
@@ -2,24 +2,33 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const path = require("path");
+let ejs = require('ejs');
 const io = require("socket.io")(http);
 const port = process.env.PORT || 5000;
 const supabase = require("./supabase.js");
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.use(express.static(path.join(__dirname, "public")));
 
 let wordArray;
 
 app.get("/", async (req, res) => {
-  console.log("HOI");
+  // console.log("HOI");
+  res.render('index');
+  // res.sendFile(path.resolve(__dirname, "views"));
+});
+
+app.get("/draw", async (req, res) => {
   wordArray = await supabase.from("drawing-words").select();
   console.log(wordArray.data);
-  res.sendFile(path.resolve(__dirname, "views/index.html"));
-
   let randomWord = Math.floor(Math.random() * wordArray.data.length);
-
+  res.locals.word = wordArray.data[randomWord].word;
+  res.render('draw');
   console.log(wordArray.data[randomWord]);
-});
+})
 
 io.on("connection", (socket) => {
   console.log("a user connected");
@@ -31,6 +40,10 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
+
+  socket.on("drawing", draw=>{
+    io.emit("drawing", draw)
+  })
 });
 
 http.listen(port, () => {
--- public/script.js
+++ public/script.js
@@ -2,20 +2,44 @@ let socket = io();
 let messages = document.querySelector(".chat ul");
 let chatInput = document.querySelector(".chat form input");
 
+
+// Get username 
+
+let search = window.location.search;
+
+let params = new URLSearchParams(search);
+let username = params.get('username');
+
+console.log(username);
+
+// Chat section
+
 document.querySelector(".chat form").addEventListener("submit", (event) => {
   event.preventDefault();
   if (chatInput.value) {
-    socket.emit("message", chatInput.value);
+    socket.emit("message", {text: chatInput.value, name: username});
     chatInput.value = "";
   }
 });
 
 socket.on("message", (message) => {
-  messages.appendChild(
-    Object.assign(document.createElement("li"), { textContent: message })
-  );
+  let li = document.createElement('li');
+  li.textContent = message.text;
+  li.setAttribute('username', message.name);
+
+  messages.insertAdjacentElement('beforeend', li);
+
   messages.scrollTop = messages.scrollHeight;
 });
+
+// // Join chatroom
+// socket.emit('joinRoom', { username });
+
+// // Get room and users
+// socket.on('roomUsers', ({ users }) => {
+//   outputUsers(users);
+//   console.log(username);
+// });
 
 // Canvas section
 
@@ -24,6 +48,9 @@ const context = paintCanvas.getContext("2d");
 context.lineCap = "round";
 
 const colorPicker = document.querySelector(".js-color-picker");
+
+
+socket.on('drawing', drawEvent)
 
 colorPicker.addEventListener("change", (event) => {
   context.strokeStyle = event.target.value;
@@ -38,13 +65,24 @@ lineWidthRange.addEventListener("input", (event) => {
   context.lineWidth = width;
 });
 
+context.lineWidth = lineWidthRange.value
+
 let x = 0,
   y = 0;
 let isMouseDown = false;
 
-const stopDrawing = () => {
+const stopDrawing = (e) => {
+  if(!isMouseDown) return
   isMouseDown = false;
+  drawLine(e)
 };
+
+const onMouseMove = (e)=>{
+  console.log(isMouseDown);
+  if(!isMouseDown) return
+  drawLine(e)
+}
+
 const startDrawing = (event) => {
   isMouseDown = true;
   [x, y] = [event.offsetX, event.offsetY];
@@ -60,10 +98,42 @@ const drawLine = (event) => {
     //[x, y] = [newX, newY];
     x = newX;
     y = newY;
+
+    socket.emit('drawing', {
+      x: newX,
+      y: newY,
+      stroke: context.lineWidth,
+      color: context.strokeStyle
+    });
   }
 };
 
-paintCanvas.addEventListener("mousedown", startDrawing);
-paintCanvas.addEventListener("mousemove", drawLine);
-paintCanvas.addEventListener("mouseup", stopDrawing);
-paintCanvas.addEventListener("mouseout", stopDrawing);
+function throttle(callback, delay) {
+  var previousCall = new Date().getTime();
+  return function() {
+    var time = new Date().getTime();
+
+    if ((time - previousCall) >= delay) {
+      previousCall = time;
+      callback.apply(null, arguments);
+    }
+  };
+}
+
+paintCanvas.addEventListener("mousedown", startDrawing, false);
+paintCanvas.addEventListener("mousemove", throttle(onMouseMove, 1), false);
+paintCanvas.addEventListener("mouseup", stopDrawing, false);
+paintCanvas.addEventListener("mouseout", stopDrawing, false);
+
+function drawEvent(draw) {
+  // console.log(draw);
+  // context.drawLine()
+  const {x,y, color, stroke} = draw
+  context.beginPath();
+  context.moveTo(x, y);
+  context.lineTo(x, y);
+  context.strokeStyle = color;
+  context.lineWidth = stroke;
+  context.stroke();
+  context.closePath();
+}
